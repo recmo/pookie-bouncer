7e0c6e58fc22ed5686c4fca2b7e624e8283b36c6
Remco Bloemen <remco@0x.org>
2020-12-05 19:34:17 -0800

    ADC interupt and Kalman filter

--- Readme.md
+++ Readme.md
@@ -30,6 +30,7 @@ the existing. Then convince mac to execute it.
   * [oled api doc](https://github.com/HelTecAutomation/Heltec_ESP32/blob/master/src/oled/API.md)
 * [ESP32 Datasheet](https://www.espressif.com/sites/default/files/documentation/esp32_datasheet_en.pdf)
   * [API docs](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference)
+  * [hal source](https://github.com/espressif/esp-idf)
 * BIQU A4988 Compatible motor driver modules
    * [vendor](https://www.biqu.equipment/products/1pcs-3d-printer-kit-a4988-stepper-motor-driver-module-with-heatsinks-reprap-board-for-3d-printer-free-shipping)
    * [A4988 datasheet](https://www.pololu.com/file/0J450/A4988.pdf)
@@ -38,4 +39,7 @@ the existing. Then convince mac to execute it.
   * [vendor](https://www.omc-stepperonline.com/nema-17-stepper-motor/nema-17-bipolar-59ncm-84oz-in-2a-42x48mm-4-wires-w-1m-cable-and-connector.html?mfp=146-rated-current-a%5B2.00%2C2%2C2.0%2C2.10%2C2.1%2C2.3%5D)
   * [datasheet](https://www.omc-stepperonline.com/download/17HS19-2004S1.pdf)
   * [torque curve](https://www.omc-stepperonline.com/download/17HS19-2004S1_Torque_Curve.pdf)
-* [AccelStepper API docs](http://www.airspayce.com/mikem/arduino/AccelStepper/classAccelStepper.html)
+* [ESP32 based stepper controller](https://github.com/bdring/Grbl_Esp32)
+* [Simple Kalman Filter library](https://github.com/denyssene/SimpleKalmanFilter)
+* [Sampling ADC from interupt](https://www.toptal.com/embedded/esp32-audio-sampling)
+
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,92 +1,173 @@
 #include <heltec.h>
 #include <driver/adc.h>
-#include <AccelStepper.h>
+#include <driver/rmt.h>
+#include <soc/sens_reg.h>
+#include <soc/sens_struct.h>
+#include <SimpleKalmanFilter.h>
 
 #define PIN_POT1 36
 #define PIN_POT2 37
 #define PIN_POT3 38
 #define PIN_POT4 39
 #define PIN_DIR  12
-#define PIN_STEP 13
+#define PIN_STEP GPIO_NUM_13
 
-AccelStepper stepper(AccelStepper::DRIVER, PIN_STEP, PIN_DIR);
+rmt_config_t step_rmt_config;
+
+rmt_item32_t step_rmt_items[] = {
+  // duration, level, duration, level (duration 15 bit in 1 μs ticks)
+  {{{200, 1, 200, 0}}}, // 200 μs HIGH, 200 μs LOW
+};
+
+// SimpleKalmanFilter(e_mea, e_est, q);
+// e_mea: Measurement Uncertainty 
+// e_est: Estimation Uncertainty 
+// q: Process Noise
+
+uint32_t pot_index = 0;
+uint16_t pot_samples[4][64];
+int last_time = 0;
+
+const float pot_scale = 1.0 / 4096.0;
+
+struct kalman_filter_t {
+  float estimate;
+  float err_estimate;
+};
+const float kalman_measurement_error = 0.05; // Sensor noise
+const float kalman_q = 0.02; // Process noise
+kalman_filter_t kf[] = {
+  {0.5, kalman_measurement_error},
+  {0.5, kalman_measurement_error},
+  {0.5, kalman_measurement_error},
+  {0.5, kalman_measurement_error},
+};
+
+hw_timer_t* adc_timer = NULL;
+portMUX_TYPE DRAM_ATTR timer_mutex = portMUX_INITIALIZER_UNLOCKED; 
+
+
+#define ADC_SAMPLES_COUNT 1024 // Must be power of two
+uint16_t adc_buffer[ADC_SAMPLES_COUNT][4];
+uint16_t adc_write = 0;
+uint16_t adc_read = 0;
+
+void kalman_update(kalman_filter_t* filter, float value) {
+  float kalman_gain = filter->err_estimate / (filter->err_estimate + kalman_measurement_error);
+  float delta_est = kalman_gain * (value - filter->estimate);
+  filter->estimate += delta_est;
+  
+  if (delta_est < 0.0) {
+    delta_est = -delta_est;
+  }
+  filter->err_estimate *= 1.0 - kalman_gain;
+  filter->err_estimate += delta_est * kalman_q;
+}
+
+// Simplified version of `adc1_get_raw` for IRAM use
+// Requires adc1_get_raw to be called at least once to do the setup
+int IRAM_ATTR iram_adc1_get_raw(int channel) {
+    uint16_t adc_value;
+    SENS.sar_meas_start1.sar1_en_pad = (1 << channel); // only one channel is selected
+    while (SENS.sar_slave_addr1.meas_status != 0);
+    SENS.sar_meas_start1.meas1_start_sar = 0;
+    SENS.sar_meas_start1.meas1_start_sar = 1;
+    while (SENS.sar_meas_start1.meas1_done_sar == 0);
+    adc_value = SENS.sar_meas_start1.meas1_data_sar;
+    return adc_value;
+}
+
+void IRAM_ATTR on_timer() {
+   portENTER_CRITICAL_ISR(&timer_mutex);
+   
+   // Read pots to ring buffer
+   adc_buffer[adc_write][0] = iram_adc1_get_raw(ADC1_CHANNEL_0);
+   adc_buffer[adc_write][1] = iram_adc1_get_raw(ADC1_CHANNEL_1);
+   adc_buffer[adc_write][2] = iram_adc1_get_raw(ADC1_CHANNEL_2);
+   adc_buffer[adc_write][3] = iram_adc1_get_raw(ADC1_CHANNEL_3);
+   adc_write += 1;
+   adc_write &= ADC_SAMPLES_COUNT - 1;
+   
+   portEXIT_CRITICAL_ISR(&timer_mutex);
+}
 
 void setup() {
-  pinMode(LED,OUTPUT);
-  digitalWrite(LED,HIGH);
 
   Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);
-
   delay(300);
+
+  // Conf
   Heltec.display->clear();
   Heltec.display->setFont(ArialMT_Plain_10);
   Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
   Heltec.display->drawString(0, 0, "Start Measuring....");
   Heltec.display->display();
 
+  // Configure ADC for reading pots
   adc1_config_width(ADC_WIDTH_BIT_12);
   adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_MAX);
   adc1_config_channel_atten(ADC1_CHANNEL_1, ADC_ATTEN_MAX);
   adc1_config_channel_atten(ADC1_CHANNEL_2, ADC_ATTEN_MAX);
   adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_MAX);
+  // Fetch now so any setup is done and we can use iram_adc1_get_raw
+  adc1_get_raw(ADC1_CHANNEL_0);
+  adc1_get_raw(ADC1_CHANNEL_1);
+  adc1_get_raw(ADC1_CHANNEL_2);
+  adc1_get_raw(ADC1_CHANNEL_3);
 
-  // set the maximum speed, acceleration factor,
-  // initial speed and the target position
-  stepper.setMaxSpeed(32000); // 600 rpm = 10 hz * 200 * 16
-  stepper.setAcceleration(32000 / 0.2); // 100% speed in 0.2 sec
-  stepper.setSpeed(32000); // 100%
-  stepper.moveTo(200 * 16 / 2);
+  // Configure RMT for driving STEP pulses
+  step_rmt_config.rmt_mode = RMT_MODE_TX;
+  step_rmt_config.channel = RMT_CHANNEL_0;
+  step_rmt_config.gpio_num = PIN_STEP;
+  step_rmt_config.mem_block_num = 1;
+  step_rmt_config.tx_config.loop_en = 1; // Repeat
+  step_rmt_config.tx_config.carrier_en = 0;
+  step_rmt_config.tx_config.idle_output_en = 1;
+  step_rmt_config.tx_config.idle_level = RMT_IDLE_LEVEL_LOW;
+  step_rmt_config.tx_config.carrier_level = RMT_CARRIER_LEVEL_HIGH;
+  step_rmt_config.clk_div = 80; // 80M Hz / 80 = 1MHz or 1 μs per tick
+  rmt_config(&step_rmt_config);
+  rmt_driver_install(step_rmt_config.channel, 0, 0);  //  rmt_driver_install(rmt_channel_t channel, size_t rx_buf_size, int rmt_intr_num)
+  
+  // Spin motor
+  rmt_write_items(step_rmt_config.channel, step_rmt_items, 1, 0);
+
+  // Configure timer interupt
+  adc_timer = timerBegin(3, 80, true); // 80 MHz / 80 = 1 MHz hardware clock for easy figuring
+  timerAttachInterrupt(adc_timer, &on_timer, true); // Attaches the handler function to the timer 
+  timerAlarmWrite(adc_timer, 1000, true); // Interrupts at 1 Mhz / 1000 = 1 kHz
+  timerAlarmEnable(adc_timer);
 }
 
-uint32_t pot_index = 0;
-uint16_t pot_samples[4][64];
-int last_time = 0;
+
+char buffer[100];
 
 void loop() {
-  // Update POTS
-  pot_samples[0][pot_index] = adc1_get_raw(ADC1_CHANNEL_0);
-  pot_samples[1][pot_index] = adc1_get_raw(ADC1_CHANNEL_1);
-  pot_samples[2][pot_index] = adc1_get_raw(ADC1_CHANNEL_2);
-  pot_samples[3][pot_index] = adc1_get_raw(ADC1_CHANNEL_3);
-  pot_index += 1;
-  pot_index &= 63;
+  // Serial.println(adc_write);
 
-  // Compute moving average
-  uint32_t val0 = 0;
-  uint32_t val1 = 0;
-  uint32_t val2 = 0;
-  uint32_t val3 = 0;
-  for (uint32_t i = 0; i < 64; i++) {
-    val0 += pot_samples[0][i];
-    val1 += pot_samples[1][i];
-    val2 += pot_samples[2][i];
-    val3 += pot_samples[3][i];
+  // Read the ADC ring buffer and update Kalman filters
+  while (adc_read != adc_write) {
+    kalman_update(&kf[0], pot_scale *adc_buffer[adc_read][0]);
+    kalman_update(&kf[1], pot_scale *adc_buffer[adc_read][1]);
+    kalman_update(&kf[2], pot_scale *adc_buffer[adc_read][2]);
+    kalman_update(&kf[3], pot_scale *adc_buffer[adc_read][3]);
+    adc_read += 1;
+    adc_read &= ADC_SAMPLES_COUNT - 1;
   }
-  float pot0 = val0 / (64.0 * 4096.0);
-  float pot1 = val1 / (64.0 * 4096.0);
-  float pot2 = val2 / (64.0 * 4096.0);
-  float pot3 = val3 / (64.0 * 4096.0);
 
-  // Compute loop time
-  int time_now = millis();
-  int loop_time = time_now - last_time;
-  last_time = time_now;
-  Serial.println(loop_time);
-
-  /*
-  char buffer[100];
-  sprintf(buffer, "%.4f %.4f\n%.4f %.4f\n%d", pot0, pot1, pot2, pot3, loop_time);
+  sprintf(buffer, "%.4f %.4f %.4f %.4f\n", kf[0].estimate, kf[1].estimate, kf[2].estimate, kf[3].estimate);
   Heltec.display->clear();
   Heltec.display->drawString(0, 0, buffer);
   Heltec.display->display();
-  */
-  
-  // Spin motor
-  if (stepper.distanceToGo() == 0) 
-    stepper.moveTo(-stepper.currentPosition());
-    
-  // Move the motor one step
-  stepper.run();
+  Serial.print(buffer);
+
+  // Update motor speed
+  // 1 rps => 200 * 16 = 32000 pps => 31.5 μspp
+  step_rmt_items[0].duration0 = 20 + 400 * 0.5;
+  step_rmt_items[0].duration1 = 20 + 400 * 0.5;
+  rmt_write_items(step_rmt_config.channel, step_rmt_items, 1, 0);
+
+  delay(10);
 }
 
 double read_pot(byte pin){
