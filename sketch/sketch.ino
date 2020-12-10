7b8479c94195442ed1770e29cf488c565c809db3
Remco Bloemen <remco@0x.org>
2020-12-10 12:27:01 -0800

    TMC2209 UART

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,6 +1,7 @@
 #include <heltec.h>
 #include <driver/adc.h>
 #include <driver/rmt.h>
+#include <driver/uart.h>
 #include <soc/sens_reg.h>
 #include <soc/sens_struct.h>
 #include <soc/rmt_reg.h>
@@ -9,8 +10,9 @@
 #include "pots.h"
 
 #define TAU 6.283185307179586
-#define PIN_DIR  12
+#define PIN_DIR  GPIO_NUM_12
 #define PIN_STEP GPIO_NUM_13
+#define PIN_UART GPIO_NUM_14
 #define RMT_TX_CHANNEL (RMT_CHANNEL_0)
 
 char buffer[100];
@@ -21,7 +23,7 @@ char buffer[100];
 
 #define MOTOR_BUFFER_LEN 1024 // Must be power of two
 uint16_t motor_buffer[MOTOR_BUFFER_LEN];
-uint16_t motor_read = 0;
+uint16_t motor_read = MOTOR_BUFFER_LEN - 1;
 uint16_t motor_write = 0;
 
 // max pulses per second = 10 * 200 * 16 = 32 kHz = 31.25 us
@@ -54,6 +56,25 @@ void IRAM_ATTR on_rmt(void* arg) {
   }
 }
 
+
+void swuart_calcCRC(uint8_t* datagram, uint8_t datagramLength) {
+  int i,j;
+  uint8_t* crc = datagram + (datagramLength - 1); // CRC located in last byte of message
+  uint8_t currentByte;
+  *crc = 0;
+  for (i = 0; i < (datagramLength-1); i++) {    // Execute for all bytes of a message
+    currentByte = datagram[i];                  // Retrieve a byte to be sent from Array
+    for (j = 0; j < 8; j++) {
+      if ((*crc >> 7) ^ (currentByte & 0x01) ) { // update CRC based result of XOR operation
+        *crc = (*crc << 1) ^ 0x07;
+      } else {
+        *crc = (*crc << 1);
+      }
+      currentByte = currentByte >> 1;
+    } // for CRC bit
+  } // for message byte}
+}
+
 void setup() {
   Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);
   delay(300);
@@ -78,6 +99,35 @@ void setup() {
   double max_rpm       = 600;  // rpm
   double max_torque    = 30;   // N cm
   double inertial_mass = 82.0; // g cm2
+
+  // Configure UART
+  Serial2.begin(115200, SERIAL_8N1, PIN_UART, PIN_UART);
+  uint8_t datagram[] = { 0x50, 0x40, 0x00, 0x00 };
+  swuart_calcCRC(datagram, 4);
+  Serial2.write(datagram, 4);
+  delay(100);
+  Serial2.write(datagram, 4);
+  delay(100);
+  
+  /*
+  uart_config_t uart_config = {
+    .baud_rate = 115200,
+    .data_bits = UART_DATA_8_BITS,
+    .parity = UART_PARITY_DISABLE,
+    .stop_bits = UART_STOP_BITS_1,
+    .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
+    .rx_flow_ctrl_thresh = 122,
+  };
+  // Configure UART parameters
+  ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_config));
+  ESP_ERROR_CHECK(uart_set_pin(UART_NUM_0, PIN_UART, PIN_UART, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+  // Setup UART buffered IO with event queue
+  const int uart_buffer_size = (1024 * 2);
+  QueueHandle_t uart_queue;
+  // Install UART driver using an event queue here
+  ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0, uart_buffer_size, \
+                                          uart_buffer_size, 10, &uart_queue, 0));
+  */
 
   // Configure RMT for driving STEP pulses
   rmt_config_t config = {
@@ -106,7 +156,7 @@ void setup() {
 
   // Write motor buffer
   for (int j = 0; j < MOTOR_BUFFER_LEN; j++)
-    motor_buffer[j] = 2000;
+    motor_buffer[j] = MAX_DURATION;
 
   // Load buffer
   on_rmt(NULL);
@@ -166,23 +216,63 @@ inline void write_pulse(bool step_direction, uint16_t duration) {
   motor_direction = step_direction;
 }
 
+float amplitude = 1.0;
+float omega = 1.0;
+
+float trajectory(uint32_t t) {
+  // TODO: Don't use instantenous velocity but solve over range.
+  float t_sec = t * 1e-7 * omega; // Time in seconds
+  float revolutions = amplitude * sin(t_sec); // Position in revolutions
+  float steps = revolutions * 3200.0; // Position in steps
+  return steps;
+}
+
+uint tick = 0;
+
 void loop() {
   // Update potentiometer readings
   update_pots();
 
-  Serial.println((motor_read - motor_write) & (MOTOR_BUFFER_LEN - 1));
+  //Serial.println((motor_read - motor_write) & (MOTOR_BUFFER_LEN - 1));
 
-  while (can_write_pulse()) {
-    // TODO: Don't use instantenous velocity but solve over range.
-    float t_sec = motor_time * 1e-7; // Time in seconds
-    float omega = POT(0) * cos(t_sec); // Revolutions per second
-    float sps = omega * 32000; // Steps per second
-    float hnsps = 1.0e7 / sps; // 100ns per step
-    if (hnsps < 0) {
-      write_pulse(false, -hnsps);
-    } else {
-      write_pulse(true, hnsps);
+  // UART
+  if (Serial2.available()) {
+    Serial.print("UART: ");
+    while (Serial2.available()) {
+      uint8_t rbyte = Serial2.read();
+      sprintf(buffer, "%02x ", rbyte);
+      Serial.print(buffer);
     }
+    Serial.println("");
+  }
+
+  while (can_write_pulse()) { // TODO: Don't write more than ~100 ms
+    if (motor_time > M_PI * 2e7 / omega) {
+      motor_time -= M_PI * 2e7 / omega;
+      // This is the time to commit to the new amplitude and omega.
+      amplitude = 3.0 * POT(0);
+      omega = 10.0 * POT(1);
+    }
+
+    float current_position = trajectory(motor_time);
+    uint16_t duration = MAX_DURATION;
+    float new_position = trajectory(motor_time + duration);
+    bool step_direction = new_position > current_position;
+    if (fabs(new_position - current_position) > 1) {
+      duration = float(duration) / fabs(new_position - current_position);
+      if (false && duration < MAX_DURATION / 4) {
+        duration *= 2;
+        float new_position = trajectory(motor_time + duration);
+        duration = float(duration) / fabs(new_position - current_position);
+      }
+    }
+    // TODO: Use float to compute more accurate position.
+    if (motor_time - tick > 100000) { // 100 Hz
+      // Serial.println(duration);
+      tick = motor_time;
+    }
+    write_pulse(step_direction, duration);
+    // write_pulse(true, MAX_DURATION);
   }
 
   // Display potentiometer readings
---
+++ uart-test/uart-test.ino
@@ -0,0 +1,97 @@
+#include <heltec.h>
+#include <driver/uart.h>
+
+// UART unfortunately doesn't seem to run on a single
+// INPUT_OUTPUT_OD pin. Instead we need to do this
+// externally by connecting a resistor between TX and
+// RX and connecting RX to the device.
+#define PIN_UART_RX GPIO_NUM_14
+#define PIN_UART_TX GPIO_NUM_27
+
+char buffer[100];
+
+void swuart_calcCRC(uint8_t* datagram, uint8_t datagramLength) {
+  int i,j;
+  uint8_t* crc = datagram + (datagramLength - 1); // CRC located in last byte of message
+  uint8_t currentByte;
+  *crc = 0;
+  for (i = 0; i < (datagramLength-1); i++) {    // Execute for all bytes of a message
+    currentByte = datagram[i];                  // Retrieve a byte to be sent from Array
+    for (j = 0; j < 8; j++) {
+      if ((*crc >> 7) ^ (currentByte & 0x01) ) { // update CRC based result of XOR operation
+        *crc = (*crc << 1) ^ 0x07;
+      } else {
+        *crc = (*crc << 1);
+      }
+      currentByte = currentByte >> 1;
+    } // for CRC bit
+  } // for message byte}
+}
+
+void send(uint8_t* datagram, uint8_t len) {
+  // Add CRC
+  swuart_calcCRC(datagram, 4);
+
+  // Log
+  Serial.print("TX ");
+  for (int i = 0; i < len; i++) {
+    sprintf(buffer, "%02x ", datagram[i]);
+    Serial.print(buffer);
+  }
+  Serial.println("");
+
+  // Send
+  Serial1.write(datagram, 4);
+}
+
+void receive() {
+  if (Serial1.available()) {
+    Serial.print("RX ");
+    while (Serial1.available()) {
+      uint8_t rbyte = Serial1.read();
+      sprintf(buffer, "%02x ", rbyte);
+      Serial.print(buffer);
+    }
+    Serial.println("");
+  }
+}
+
+uint32_t read_register(uint8_t reg) {
+  uint8_t datagram[] = { 0x05, 0x00, reg, 0x00 };
+  send(datagram, 4);
+
+  //
+  uint8_t reply[4 + 8];
+  Serial.print("Reply: ");
+  Serial1.readBytes(reply, 8);
+  for (int i = 0; i < 12; i++) {
+    sprintf(buffer, "%02x ", reply[i]);
+    Serial.print(buffer);
+  }
+  Serial.println("");
+  return 0;
+}
+
+void setup() {
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);
+  delay(300);
+  Serial.println("Booting...");
+
+  // Display
+  Heltec.display->clear();
+  Heltec.display->setFont(ArialMT_Plain_10);
+  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+  Heltec.display->drawString(0, 0, "UART test");
+  Heltec.display->display();
+  
+  Serial1.begin(115200, SERIAL_8N1, PIN_UART_RX, PIN_UART_TX);
+  Serial1.setTimeout(10);
+}
+
+uint8_t datagram[] = { 0x05, 0x00, 0x06, 0x00 };
+
+void loop() {
+  read_register(0x06);
+    
+  delay(1000);
+}
